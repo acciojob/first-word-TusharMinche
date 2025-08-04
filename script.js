5572c5f7d472ec79f1882d0b4245f7414dd7dfe1
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-04 18:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+
+	if(s.length == 0) return s;
+	s = s.trim();
+
+	let ans = "";
+
+	for(let ch of s){
+		if(ch == ' ') break;
+		ans += ch;
+	}
+
+	return ans;
 }
 
 // Do not change the code below
